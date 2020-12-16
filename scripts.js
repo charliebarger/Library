af5b6a1a99ba6543bc9cddfc91edd4c205c3dff6
charliebarger <charliebarger04@gmail.com>
2020-12-16 08:19:49 -0700

    cleaned up formatting and added comments

--- scripts.js
+++ scripts.js
@@ -8,31 +8,31 @@ let placement = 0;
 const deleteIt = document.getElementById("delete")
 let readbuttons;
 let wrapper;
+let splicePlace = []
 
 let myLibrary = localStorage.getItem('library')
   ? JSON.parse(localStorage.getItem('library'))
   : []
-
 localStorage.setItem('library', JSON.stringify(myLibrary))
 const data = JSON.parse(localStorage.getItem('library'))
 
+//constructor
 function Book(author, title, pages, read){
     this.author = author;
     this.title = title
     this.pages = pages
     this.read = read
-    
 }
 
+//create new instance of Book and add to myLibrary
 function addBooktoLibrary(title, author, pages, readNotread){
     let newBook = new Book(title, author, pages, readNotread);
     myLibrary.push(newBook)
 }
 
-
+//remove all book then call append for myLibrary items in reverse order (so that last added is appended to the top)
 function display(){
-    localStorage.clear()
-    localStorage.setItem('library', JSON.stringify(myLibrary))
+    setLocalStorage()
     const displayedBooks = document.querySelectorAll('.mybooks')
     displayedBooks.forEach((book) => book.remove())
     let x = myLibrary.length -1 ;
@@ -40,81 +40,117 @@ function display(){
         let bookPosition = myLibrary[x]
         wrapper = createWrapper(myLibrary[x])
         for (item in bookPosition) {
-            appendBooks(bookPosition, bookPosition[item], wrapper)
+            appendBookitems(bookPosition, bookPosition[item], wrapper)
         }
     }
 }
 
-let splicePlace = []
-function createWrapper(item){
-        const wrapper = document.createElement("section");
-        wrapper.classList.add("mybooks");
-        bookTable.appendChild(wrapper);
-        const checkbox = document.createElement('input');
-        checkbox.addEventListener('change', () => {
-            if (checkbox.checked){
-                splicePlace.push(myLibrary.indexOf(item))
-            }
-            else {
-                let index = splicePlace.indexOf(myLibrary.indexOf(item))
-                splicePlace.splice(index, 1)
-            }
-    
-    })
-        checkbox.type = "checkbox";
-        checkbox.classList.add('id','checkbox')
-        wrapper.appendChild(checkbox)
-        return(wrapper);
+//functions
+
+//clear local storage and set it to myLibrary
+function setLocalStorage(){
+    localStorage.clear()
+    localStorage.setItem('library', JSON.stringify(myLibrary))
 }
 
+//create a wrapper, add a checkbox to it, and return it
+function createWrapper(book){
+    const wrapper = document.createElement("section");
+    wrapper.classList.add("mybooks");
+    bookTable.appendChild(wrapper);
+    createCheckbox(wrapper, book)
+    return(wrapper);
+}
 
-function appendBooks(item, value, wrapper){
+function createCheckbox(wrapper, book){
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.classList.add('id','checkbox')
+    wrapper.appendChild(checkbox)
+    checkboxListner(checkbox, book)
+}
+
+//records the indexOf an item in my library and appends it to the array, splicePlace when checked and removes it when unchecked
+function checkboxListner(checkbox, book){
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked){
+            splicePlace.push(myLibrary.indexOf(book))
+        }
+        else {
+            let index = splicePlace.indexOf(myLibrary.indexOf(book))
+            splicePlace.splice(index, 1)
+        }
+    })
+}
+
+//appends book items to the book wrapper
+function appendBookitems(book, value, wrapper){
     if (value == 'Read'|| value == 'Not Read'){
-            const readButton = document.createElement("button")
-            readButton.classList.add('readButtons')
-            readButton.textContent = value;
-            value == 'Read' ?  readButton.style.background = 'green' :  readButton.style.background = 'red';
-            wrapper.appendChild(readButton)
-            readButton.addEventListener('click', function(){
-                if (item.read == 'Read'){
-                    item.read = 'Not Read'
-                }
-                else{
-                    item.read = 'Read'
-                }
-                display()
-            })
+        createReadbutton(book, value, wrapper)
     }
     else{
-        const bookDetail = document.createElement("span")
-        bookDetail.textContent = value;
-        wrapper.appendChild(bookDetail);
+        createBookContent(value, wrapper)
     }
 }
 
+function createBookContent(value, wrapper){
+    const bookDetail = document.createElement("span")
+    bookDetail.textContent = value;
+    wrapper.appendChild(bookDetail);
+}
+
+//create and appends read button, and changes color based on value 
+function createReadbutton(book, value, wrapper){
+    const readButton = document.createElement("button")
+    readButton.classList.add('readButtons')
+    readButton.textContent = value;
+    value == 'Read' ? readButton.setAttribute("style", "background-color: green ; border: green solid 1px") :  readButton.style.background = 'red';
+    wrapper.appendChild(readButton)
+    readButtonListner(readButton, book)
+}
+
+//changes read value inside of particular book in myLibrary and calls display agian
+function readButtonListner(button, book){
+    button.addEventListener('click', function(){
+    if (book.read == 'Read'){
+        book.read = 'Not Read'
+    }
+    else{
+        book.read = 'Read'
+    }
+    display()
+    })
+}
+
+//on load display each item in data (variable holding js readable LocalStorage)
+data.forEach((item) => {
+    display()
+})
+
+//clear the inputs
 function clearForm(){
     title.value = author.value = pages.value = ""
 }
 
- submit.addEventListener('click', function(e) {
-     if (!title.value || !author.value || !pages.value){
-         return
-     }
-     e.preventDefault()
-     addBooktoLibrary(title.value, author.value, pages.value, readNotread.value)
-     display()
-     clearForm()
-     
-    })
+//event Listeners
 
-let deleteClicked = false;
- deleteIt.addEventListener('click', function() {
+//if any values are false return in order to retain default functionality 
+submit.addEventListener('click', function(e) {
+    if (!title.value || !author.value || !pages.value){
+        return
+    }
+    e.preventDefault()
+    addBooktoLibrary(title.value, author.value, pages.value, readNotread.value)
+    display()
+    clearForm()
+})
+
+//sort and reverse splice order so that splice by numbers is not disrupted by changing item positions in myLibrary
+deleteIt.addEventListener('click', function() {
+    splicePlace.sort().reverse()
     splicePlace.forEach((place) => {
         myLibrary.splice(place, 1)
     })
+    splicePlace = []
     display()
- })
-
- data.forEach((item) => {
-  display()
 })
